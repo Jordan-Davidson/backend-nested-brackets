f7c9aaad26005fb6fee550c79027bca1b4656f87
Jordan Davidson <jordandavidson744@yahoo.com>
2020-02-11 15:11:38 -0500

    setting up files

--- nested.py
+++ nested.py
@@ -3,21 +3,26 @@
 """
 Module docstring: One line description of what your program does.
 """
-__author__ = "???"
+__author__ = "Jordan"
 
 import sys
 
 
 def is_nested(line):
-    """Validate a single input line for correct nesting"""
-    pass
+    print('hello')
+    return 'hello'
 
 
 def main(args):
+    with open('nested.py', 'r') as f:
+        for line in f.readlines():
+            final = is_nested(line)
+            with open('output', 'w') as o:
+                o.write(final)
+
+            
     """Open the input file and call `is_nested()` for each line"""
     # Results: print to console and also write to output file
-    pass
-
 
 if __name__ == '__main__':
     main(sys.argv[1:])
